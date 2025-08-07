d1ea4ad85987fb2b63589ca83e243d1e6b17c714
hsa00000 <hsa00000@gmail.com>
2025-08-07 12:43:24 +0800

    refactor: simplify basicString usage in UniversalPage component

--- gallery-frontend/src/components/Page/UniversalPage.vue
+++ gallery-frontend/src/components/Page/UniversalPage.vue
@@ -1,16 +1,9 @@
 <template>
-  <HomeMain :basic-string="basicString" />
+  <HomeMain :basic-string="basicStringStore.basicString" />
 </template>
 
 <script setup lang="ts">
-import { computed } from 'vue'
 import HomeMain from '@/components/Home/HomeMain.vue'
 import { useBasicStringStore } from '@/store/basicStringStore'
-
 const basicStringStore = useBasicStringStore('mainId')
-
-// 从 store 获取 basicString
-const basicString = computed(() => {
-  return basicStringStore.basicString
-})
 </script>
