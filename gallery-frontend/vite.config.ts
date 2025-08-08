5634a82a6338cd2e1f5f86ff511125069afa492b
ZHI LIN <hsa00000@gmail.com>
2025-08-08 22:00:35 +0800

    feat: add vite-plugin-vue-devtools for improved Vue debugging in development

--- gallery-frontend/vite.config.ts
+++ gallery-frontend/vite.config.ts
@@ -2,10 +2,13 @@ import { fileURLToPath } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { VitePWA } from 'vite-plugin-pwa'
+import vueDevTools from 'vite-plugin-vue-devtools'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    vueDevTools(),
     VitePWA({
       srcDir: 'src/worker', // Specify Service Worker directory
       filename: 'serviceWorker.ts', // Service Worker filename
