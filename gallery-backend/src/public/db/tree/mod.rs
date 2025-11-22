a7b93067105818cab5ac29425cadafd111c67972
張誌麟 <60398736+hsa00000@users.noreply.github.com>
2025-11-22 19:59:40 +0800

    feat(database): implement from_row method for Database struct and simplify database queries

--- gallery-backend/src/public/db/tree/mod.rs
+++ gallery-backend/src/public/db/tree/mod.rs
@@ -9,10 +9,6 @@ use crate::public::structure::abstract_data::AbstractData;
 use crate::public::structure::album::Album;
 use crate::public::structure::database_struct::database::definition::Database;
 use crate::public::structure::database_struct::database_timestamp::DatabaseTimestamp;
-use crate::public::structure::database_struct::file_modify::FileModify;
-use arrayvec::ArrayString;
-use serde_json;
-use std::collections::{BTreeMap, HashSet};
 use std::sync::{Arc, LazyLock, RwLock, atomic::AtomicU64};
 
 pub struct Tree {
@@ -34,46 +30,7 @@ impl Tree {
         if let Ok(database) = conn.query_row(
             "SELECT * FROM database_with_tags WHERE hash = ?",
             [id],
-            |row| {
-                let hash: String = row.get("hash")?;
-                let size: u64 = row.get("size")?;
-                let width: u32 = row.get("width")?;
-                let height: u32 = row.get("height")?;
-                let thumbhash: Vec<u8> = row.get("thumbhash")?;
-                let phash: Vec<u8> = row.get("phash")?;
-                let ext: String = row.get("ext")?;
-                let exif_vec_str: String = row.get("exif_vec")?;
-                let exif_vec: BTreeMap<String, String> =
-                    serde_json::from_str(&exif_vec_str).unwrap_or_default();
-                let tag_json: String = row.get("tag_json")?;
-                let tag_vec: Vec<String> = serde_json::from_str(&tag_json).unwrap_or_default();
-                let tag: HashSet<String> = tag_vec.into_iter().collect();
-                let album_str: String = row.get("album")?;
-                let album_vec: Vec<String> = serde_json::from_str(&album_str).unwrap_or_default();
-                let album: HashSet<ArrayString<64>> = album_vec
-                    .into_iter()
-                    .filter_map(|s| ArrayString::from(&s).ok())
-                    .collect();
-                let alias_str: String = row.get("alias")?;
-                let alias: Vec<FileModify> = serde_json::from_str(&alias_str).unwrap_or_default();
-                let ext_type: String = row.get("ext_type")?;
-                let pending: bool = row.get::<_, i32>("pending")? != 0;
-                Ok(Database {
-                    hash: ArrayString::from(&hash).unwrap(),
-                    size,
-                    width,
-                    height,
-                    thumbhash,
-                    phash,
-                    ext,
-                    exif_vec,
-                    tag,
-                    album,
-                    alias,
-                    ext_type,
-                    pending,
-                })
-            },
+            Database::from_row,
         ) {
             Ok(AbstractData::Database(database))
         } else if let Ok(album) =
@@ -88,46 +45,7 @@ impl Tree {
     pub fn load_all_databases_from_db(&self) -> Result<Vec<Database>> {
         let conn = self.get_connection()?;
         let mut stmt = conn.prepare("SELECT * FROM database_with_tags")?;
-        let rows = stmt.query_map([], |row| {
-            let hash: String = row.get("hash")?;
-            let size: u64 = row.get("size")?;
-            let width: u32 = row.get("width")?;
-            let height: u32 = row.get("height")?;
-            let thumbhash: Vec<u8> = row.get("thumbhash")?;
-            let phash: Vec<u8> = row.get("phash")?;
-            let ext: String = row.get("ext")?;
-            let exif_vec_str: String = row.get("exif_vec")?;
-            let exif_vec: BTreeMap<String, String> =
-                serde_json::from_str(&exif_vec_str).unwrap_or_default();
-            let tag_json: String = row.get("tag_json")?;
-            let tag_vec: Vec<String> = serde_json::from_str(&tag_json).unwrap_or_default();
-            let tag: HashSet<String> = tag_vec.into_iter().collect();
-            let album_str: String = row.get("album")?;
-            let album_vec: Vec<String> = serde_json::from_str(&album_str).unwrap_or_default();
-            let album: HashSet<ArrayString<64>> = album_vec
-                .into_iter()
-                .filter_map(|s| ArrayString::from(&s).ok())
-                .collect();
-            let alias_str: String = row.get("alias")?;
-            let alias: Vec<FileModify> = serde_json::from_str(&alias_str).unwrap_or_default();
-            let ext_type: String = row.get("ext_type")?;
-            let pending: bool = row.get::<_, i32>("pending")? != 0;
-            Ok(Database {
-                hash: ArrayString::from(&hash).unwrap(),
-                size,
-                width,
-                height,
-                thumbhash,
-                phash,
-                ext,
-                exif_vec,
-                tag,
-                album,
-                alias,
-                ext_type,
-                pending,
-            })
-        })?;
+        let rows = stmt.query_map([], Database::from_row)?;
         rows.collect::<Result<Vec<_>, _>>()
             .map_err(anyhow::Error::from)
     }
@@ -135,46 +53,7 @@ impl Tree {
     pub fn load_database_from_hash(&self, hash: &str) -> Result<Database> {
         let conn = self.get_connection()?;
         let mut stmt = conn.prepare("SELECT * FROM database_with_tags WHERE hash = ?")?;
-        stmt.query_row([hash], |row| {
-            let hash: String = row.get("hash")?;
-            let size: u64 = row.get("size")?;
-            let width: u32 = row.get("width")?;
-            let height: u32 = row.get("height")?;
-            let thumbhash: Vec<u8> = row.get("thumbhash")?;
-            let phash: Vec<u8> = row.get("phash")?;
-            let ext: String = row.get("ext")?;
-            let exif_vec_str: String = row.get("exif_vec")?;
-            let exif_vec: BTreeMap<String, String> =
-                serde_json::from_str(&exif_vec_str).unwrap_or_default();
-            let tag_json: String = row.get("tag_json")?;
-            let tag_vec: Vec<String> = serde_json::from_str(&tag_json).unwrap_or_default();
-            let tag: HashSet<String> = tag_vec.into_iter().collect();
-            let album_str: String = row.get("album")?;
-            let album_vec: Vec<String> = serde_json::from_str(&album_str).unwrap_or_default();
-            let album: HashSet<ArrayString<64>> = album_vec
-                .into_iter()
-                .filter_map(|s| ArrayString::from(&s).ok())
-                .collect();
-            let alias_str: String = row.get("alias")?;
-            let alias: Vec<FileModify> = serde_json::from_str(&alias_str).unwrap_or_default();
-            let ext_type: String = row.get("ext_type")?;
-            let pending: bool = row.get::<_, i32>("pending")? != 0;
-            Ok(Database {
-                hash: ArrayString::from(&hash).unwrap(),
-                size,
-                width,
-                height,
-                thumbhash,
-                phash,
-                ext,
-                exif_vec,
-                tag,
-                album,
-                alias,
-                ext_type,
-                pending,
-            })
-        })
-        .map_err(anyhow::Error::from)
+        stmt.query_row([hash], Database::from_row)
+            .map_err(anyhow::Error::from)
     }
 }
--- gallery-backend/src/public/structure/database_struct/database/definition.rs
+++ gallery-backend/src/public/structure/database_struct/database/definition.rs
@@ -1,7 +1,8 @@
 use crate::public::structure::database_struct::file_modify::FileModify;
 use arrayvec::ArrayString;
-use rusqlite::Connection;
+use rusqlite::{Connection, Row};
 use serde::{Deserialize, Serialize};
+use serde_json;
 use std::collections::{BTreeMap, HashSet};
 
 #[derive(Debug, Clone, Deserialize, Default, Serialize, PartialEq, Eq)]
@@ -72,5 +73,45 @@ impl Database {
         conn.execute(sql_create_view, [])?;
 
         Ok(())
+    }
+
+    pub fn from_row(row: &Row) -> rusqlite::Result<Self> {
+        let hash: String = row.get("hash")?;
+        let size: u64 = row.get("size")?;
+        let width: u32 = row.get("width")?;
+        let height: u32 = row.get("height")?;
+        let thumbhash: Vec<u8> = row.get("thumbhash")?;
+        let phash: Vec<u8> = row.get("phash")?;
+        let ext: String = row.get("ext")?;
+        let exif_vec_str: String = row.get("exif_vec")?;
+        let exif_vec: BTreeMap<String, String> = serde_json::from_str(&exif_vec_str).unwrap();
+        let tag_json: String = row.get("tag_json")?;
+        let tag_vec: Vec<String> = serde_json::from_str(&tag_json).unwrap();
+        let tag: HashSet<String> = tag_vec.into_iter().collect();
+        let album_str: String = row.get("album")?;
+        let album_vec: Vec<String> = serde_json::from_str(&album_str).unwrap();
+        let album: HashSet<ArrayString<64>> = album_vec
+            .into_iter()
+            .filter_map(|s| ArrayString::from(&s).ok())
+            .collect();
+        let alias_str: String = row.get("alias")?;
+        let alias: Vec<FileModify> = serde_json::from_str(&alias_str).unwrap();
+        let ext_type: String = row.get("ext_type")?;
+        let pending: bool = row.get::<_, i32>("pending")? != 0;
+        Ok(Database {
+            hash: ArrayString::from(&hash).unwrap(),
+            size,
+            width,
+            height,
+            thumbhash,
+            phash,
+            ext,
+            exif_vec,
+            tag,
+            album,
+            alias,
+            ext_type,
+            pending,
+        })
     }
 }
