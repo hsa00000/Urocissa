b20a408e1f1ce06a01a37b671b9816fb6350c288
張誌麟 <60398736+hsa00000@users.noreply.github.com>
2025-11-23 13:27:44 +0800

    feat(database): ensure database directory is created before opening connection

--- gallery-backend/src/public/db/sqlite.rs
+++ gallery-backend/src/public/db/sqlite.rs
@@ -2,6 +2,7 @@ use rusqlite::Connection;
 use std::time::Duration;
 
 pub fn init_db_file_once() -> anyhow::Result<()> {
+    std::fs::create_dir_all("./db")?;
     let conn = Connection::open("./db/gallery.db")?;
     conn.busy_timeout(Duration::from_millis(5000))?;
     conn.execute_batch(
