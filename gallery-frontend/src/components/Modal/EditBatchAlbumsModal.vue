631960c82cb9dc16357c6f824b22dfa9711f0801
張誌麟 <60398736+hsa00000@users.noreply.github.com>
2025-12-24 22:23:14 +0800

    refactor: remove unused import nextTick from EditBatchAlbumsModal.vue

--- gallery-frontend/src/components/Modal/EditBatchAlbumsModal.vue
+++ gallery-frontend/src/components/Modal/EditBatchAlbumsModal.vue
@@ -105,7 +105,7 @@
 </template>
 
 <script setup lang="ts">
-import { ref, watch, computed, onMounted, nextTick } from 'vue'
+import { ref, watch, computed, onMounted } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
 import { isNavigationFailure, NavigationFailureType } from 'vue-router'
 import { useModalStore } from '@/store/modalStore'
